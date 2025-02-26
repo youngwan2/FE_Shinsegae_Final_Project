f2e1834d3b06dc864073d280f201534199d2ed40
youngwan2 <qodna25@gmail.com>
2025-02-26 20:54:10 +0900

    docs: README.md 파일 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Form from 'react-bootstrap/Form';
-import { CiHeart } from "react-icons/ci";
+import { CiHeart } from 'react-icons/ci';
 
 function App() {
   return (
     <>
-    <CiHeart />
+      <CiHeart />
       <Form
         onSubmit={(e) => {
           e.preventDefault();
@@ -15,10 +15,7 @@ function App() {
         <Form.Label htmlFor='inputPassword5'>Password</Form.Label>
         <Form.Control type='password' id='inputPassword5' aria-describedby='passwordHelpBlock' />
         <Form.Text id='passwordHelpBlock' muted>
-          Your password 
-          
-          
-          must be 8-20 characters long, contain letters and numbers, and must not
+          Your password must be 8-20 characters long, contain letters and numbers, and must not
           contain spaces, special characters, or emoji.
         </Form.Text>
       </Form>
